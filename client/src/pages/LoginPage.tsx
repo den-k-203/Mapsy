48c28eca2b7e0292039ed14551935e5dbcfe0ae9
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2023-02-26 16:12:07 +0200

    (feat) - add some styles in auth module

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -8,48 +8,53 @@ const LoginPage = () => {
   const auth = useContext(AuthContext);
   const message = useMessage();
   const navigate = useNavigate();
-  const {loading, request, error, clearError} = useHttp();
-  const [form, setForm] = useState({
-    logIdent: "",
-    password: ""
-  });
+  const { loading, request, error, clearError } = useHttp();
+  const [form, setForm] = useState({ logIdent: "", password: "" });
 
   useEffect(() => {
     if (error != null) {
       message(error);
     }
     clearError();
-  },[error, message, clearError]);
+  }, [error, message, clearError]);
 
   const changeHandler = (event: any) => {
-    setForm({...form, [event.target.name]: event.target.value});
+    setForm({ ...form, [event.target.name]: event.target.value });
   };
 
   const loginHandler = async () => {
     try {
-      const response = await request("http://localhost:5000/api/auth/login", "POST", {...form});
+      const response = await request("http://localhost:5000/api/auth/login", "POST", { ...form });
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       auth.login(response.token, response.user);
       message(response.message);
-    } catch (e) {}
+    } catch (e) {
+    }
   };
 
   return (
     <div className={"container"}>
-      <h1 style={{color: "#E1E1E1"}}>
-        <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 0 24 24" width="50px" fill="#E1E1E1">
+      <h1 style={{ color: "#E1E1E1" }} className="center-align">
+        <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 0 24 24" width="50px" fill="#8e24aa">
           <path d="M0 0h24v24H0z" fill="none" />
           <path
             d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" />
         </svg>
-        Mapsy
+        <span style={{ marginLeft: 5 }}>MAP<span style={{color:"#8e24aa"}}>SY</span></span><br />
+        <span>Авторизація</span>
       </h1>
 
       <div className="row">
-        <form className="col center-align s12">
-          <div className="row">
-            <div className="input-field col s8">
+        <form
+          className="col s6 offset-s3 center-align background grey darken-4 z-depth-1"
+          style={{
+            padding: 30,
+            borderRadius:10
+          }}
+        >
+          <div className="row center-align">
+            <div className="input-field col s12">
               <input
                 id="logIdent"
                 name="logIdent"
@@ -59,7 +64,7 @@ const LoginPage = () => {
               />
               <label htmlFor="logIdent">Логін або пошта</label>
             </div>
-            <div className="input-field col s8">
+            <div className="input-field col s12">
               <input
                 id="password"
                 name="password"
@@ -71,24 +76,23 @@ const LoginPage = () => {
             </div>
           </div>
           <button
-            className="btn waves-effect waves-light"
+            className="btn waves-effect waves-light purple darken-1"
             type="submit"
             name="action"
-            style={{marginRight:15}}
+            style={{ marginRight: 15 }}
             onClick={loginHandler}
             disabled={loading}
           >
-            Вхід
+            Увійти
           </button>
           <button
-            className="btn waves-effect waves-light"
+            className="btn waves-effect waves-light purple darken-1"
             type="submit"
             name="action"
             onClick={() => navigate("/registration")}
             disabled={loading}
           >
             Реєстрація
-            <i className="material-icons right">send</i>
           </button>
         </form>
       </div>
--- client/src/pages/RegistrationPage.tsx
+++ client/src/pages/RegistrationPage.tsx
@@ -38,18 +38,22 @@ const RegistrationPage = () => {
 
   return (
     <div className={"container"}>
-      <h1 style={{color: "#E1E1E1"}}>
-        <span style={{marginRight: 5}}>Реєстрація на</span>
-        <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 0 24 24" width="50px" fill="#E1E1E1">
+      <h1 style={{ color: "#E1E1E1" }} className="center-align">
+        <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 0 24 24" width="50px" fill="#8e24aa">
           <path d="M0 0h24v24H0z" fill="none" />
           <path
             d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" />
         </svg>
-        <span style={{marginLeft: 5}}>Mapsy</span>
+        <span style={{ marginLeft: 5 }}>MAP<span style={{color:"#8e24aa"}}>SY</span></span><br />
+        <span>Реєстрація</span>
       </h1>
 
       <div className="row" >
-        <form className="col center-align s12">
+        <form className="col s8 offset-s2 center-align background grey darken-4 z-depth-1"
+              style={{
+                padding: 30,
+                borderRadius:10
+              }}>
           <div className="row">
             <div className="input-field col s6">
               <input
@@ -113,7 +117,7 @@ const RegistrationPage = () => {
             </div>
           </div>
           <button
-            className="btn waves-effect waves-light"
+            className="btn waves-effect waves-light purple darken-1"
             type="submit"
             name="action"
             style={{marginRight:15}}
@@ -123,14 +127,13 @@ const RegistrationPage = () => {
             Вхід
           </button>
           <button
-            className="btn waves-effect waves-light"
+            className="btn waves-effect waves-light purple darken-1"
             type="submit"
             name="action"
             disabled={loading}
             onClick={registrationHandler}
           >
-            Реєстрація
-            <i className="material-icons right">send</i>
+            Зареєструватись
           </button>
         </form>
       </div>
